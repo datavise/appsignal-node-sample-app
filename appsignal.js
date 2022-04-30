34225862afc752aafa84a284d0347edd27cb3aab
Datavise <contact.deesea@gmail.com>
2022-04-30 14:03:58 +0200

    Added Application Name placeholder

--- appsignal.js
+++ appsignal.js
@@ -2,6 +2,6 @@ const { Appsignal } = require("@appsignal/nodejs");
 
 exports.appsignal = new Appsignal({
   active: true,
-  name: "node-doc",
-  pushApiKey: "..."
+  name: "YOUR APPLICATION NAME",
+  pushApiKey: "YOUR API KEY"
 });
